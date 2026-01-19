5554fb7ebb989546f7656363e66b46d5f3017966
RiteshChougule <RiteshChougule96@gmail.com>
2026-01-19 15:14:40 +0530

    added comment

--- src/main/java/com/project/login/ReportProjectApplication.java
+++ src/main/java/com/project/login/ReportProjectApplication.java
@@ -3,6 +3,7 @@ package com.project.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+//main starter file
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.project")
